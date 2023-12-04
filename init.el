89e1a30ce99caf911a826d28a775b87ca6617d8e
Sabin Pocris <dev.sabinpocris@gmail.com>
2023-12-04 15:29:10 +0200

    update setup.org

--- init.el
+++ init.el
@@ -2,3 +2,16 @@
  (expand-file-name
   "setup.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ace-window vterm org-bullets visual-fill-column ivy-rich counsel doom-modeline doom-themes company which-key rainbow-delimiters)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
